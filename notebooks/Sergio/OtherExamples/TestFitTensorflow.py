3a7bdf0a7bcd6bf5bd77c2d83be09b26abf7d004
Sergio <Sergio.Bravo.Allue@gmail.co>
2022-05-14 17:27:00 +0200

    Docs moved

--- notebooks/Sergio/OtherExamples/TestFitTensorflow.py
+++ notebooks/Sergio/OtherExamples/TestFitTensorflow.py
@@ -9,6 +9,10 @@ Original file is located at
 
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
+physical_devices = tf.config.list_physical_devices()
+print("DEVs:", physical_devices)
 
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
@@ -22,7 +26,6 @@ salida = tf.keras.layers.Dense(units=1)
 modelo = tf.keras.Sequential([oculta1, oculta2, salida])
 
 
-
 modelo.compile(
     optimizer=tf.keras.optimizers.Adam(0.1),
     loss='mean_squared_error'
@@ -32,17 +35,22 @@ print("Comenzando entrenamiento...")
 historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
 print("Modelo entrenado!")
 
-import matplotlib.pyplot as plt
+# Guardar el Modelo
+modelo.save('testModel.h5')
+print("Modelo guardado!")
+
+
 plt.xlabel("# Epoca")
 plt.ylabel("Magnitud de pérdida")
 plt.plot(historial.history["loss"])
-
-print("Hagamos una predicción!")
-resultado = modelo.predict([100.0])
-print("El resultado es " + str(resultado) + " fahrenheit!")
 
 print("Variables internas del modelo")
 #print(capa.get_weights())
 print(oculta1.get_weights())
 print(oculta2.get_weights())
 print(salida.get_weights())
+
+print("Hagamos una predicción!")
+resultado = modelo.predict([100.0])
+print("El resultado es " + str(resultado) + " fahrenheit!")
+
---
+++ notebooks/Sergio/clasification.py
@@ -0,0 +1,137 @@
+# Links
+#        https://colab.research.google.com/drive/1EjxAt_OFrdpNgCNmV15XMCE8RI3PF3sD?usp=sharing
+#        https://www.tensorflow.org/tutorials/images/classification
+
+
+
+# Es necesario pasar las imagenes a blanco y negro?
+# Normalizar las imagenes entre 0 y 1  
+# Pasar los datos a cache en lugar de disco
+import time
+start = time.time()
+
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from pathlib import Path
+
+#  Configuration
+batch_size = 8
+img_height = 84
+img_width = 84
+epochs=20
+
+print("Configuration=> Batch: " + str(batch_size) + " Size: "+ str(img_height) +"X" +  str(img_width) + " Epochs: " + str(epochs))
+
+
+data_dir = Path(os.getcwd() + "/../../data/labelled_images/")
+images = data_dir.glob('**/*.jpg')
+# print("Num images: "+ str(len(list(images))))
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+
+# print("List of class:")
+class_names = train_ds.class_names
+num_classes = len(class_names)
+# for e in class_names:
+#     print("    "+e)
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Add more images due a Unbalanced input data
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
+
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.summary()
+
+
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+model.save('testclasification.h5')
+print("Modelo guardado!")
+
+end = time.time()
+total_time = end - start
+print("Total time: "+ str(total_time))
+
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# -------------------------------
+
+
+
+
+
+
